bc01c4310464520feda2591244fe4feb12cbe6e6
Zolana Nsakala <zolana.nsakala@1und1.de>
2017-01-30 13:25:21 +0100

    adding Scene

---
+++ Project_GameJam_Wave/Assets/Scene/DeathWinButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathWinButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RestartGame()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitGame()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ Project_GameJam_Wave/Assets/Scene/MainMenuButtons.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public GameObject credits;
+	public GameObject menuScreen;
+
+	public Image playButton;
+	public Image soundButtonActive;
+	public Image soundButtonInactive;
+	public Image creditsButoon;
+
+	private bool soundActive;
+
+
+	void Start () {
+		menuScreen.SetActive (true);
+		soundButtonActive.gameObject.SetActive (true);
+		soundButtonInactive.gameObject.SetActive (false);
+		soundActive = true;
+
+	}
+
+	public void PlayButtonClick(){
+		SceneManager.LoadScene (1);
+		Debug.Log ("Scene wird geladen");
+	}
+
+	public void SettingsButtonClick(){
+		if (soundActive == true) {
+			Debug.Log ("Klappt");
+			soundActive = false;
+			soundButtonActive.gameObject.SetActive (false);
+			soundButtonInactive.gameObject.SetActive (true);
+		} else {
+			soundActive = true;
+			soundButtonActive.gameObject.SetActive (true);
+			soundButtonInactive.gameObject.SetActive (false);
+		}
+	}
+
+	public void MainMenuButtonClick(){
+		Debug.Log ("Credits geschlossen");
+		OpenMainMenu ();
+	}
+		
+	public void CreditsButtonClick(){
+		OpenCredits ();
+	}
+
+	public void OpenCredits(){
+		credits.SetActive (true);
+		menuScreen.SetActive (false);
+	}
+
+	public void OpenMainMenu(){
+		credits.SetActive (false);
+		menuScreen.SetActive (true);
+	}
+}
